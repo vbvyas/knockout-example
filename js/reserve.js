65d4f0c29e7833b03aa8e95eb47ffe1b27138ce1
Vinay <vbvyas@gmail.com>
2014-10-16 21:18:23 -0700

    Reservation view model

--- js/reserve.js
+++ js/reserve.js
@@ -1,5 +1,25 @@
-function AppViewModel() {
+// class to represent a seat row
+function SeatReservation(name, initialMeal) {
+  var self = this;
+  self.name = name;
+  self.meal = ko.observable(initialMeal);
 }
 
-// activate knockout.js
-ko.applyBindings(new AppViewModel());
+function ReservationsViewModel() {
+  var self = this;
+
+  // non-editable data
+  self.availableMeals = [
+    { mealName: "Standard (sandwich)", price: 0 },
+    { mealName: "Premium (lobster)", price: 34.95 },
+    { mealName: "Ultimate (zebra)", price: 290 }
+  ];
+
+  // editable data
+  self.seats = ko.observableArray([
+    new SeatReservation("Steve", self.availableMeals[0]),
+    new SeatReservation("Bert", self.availableMeals[0])
+  ]);
+}
+
+ko.applyBindings(new ReservationsViewModel());
