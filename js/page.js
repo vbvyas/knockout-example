f42ba3e99345ab886578756ffcd1eb94446157d0
vinay <vbvyas@gmail.com>
2014-10-21 21:06:18 -0700

    Ajax call to populate data

--- js/page.js
+++ js/page.js
@@ -2,11 +2,24 @@ function WebmailViewModel() {
   var self = this;
   self.folders = ['Inbox', 'Archive', 'Sent', 'Spam'];
   self.chosenFolderId = ko.observable();
+  self.chosenFolderData = ko.observable();
   
   // behaviors
   self.goToFolder = function (folder) {
     self.chosenFolderId(folder);
+    $.ajax({
+      url: 'http://learn.knockoutjs.com/mail?folder=' + folder,
+      type: 'GET',
+      dataType: 'jsonp',
+      crossDomain: true,
+    })
+    .done(function (data) {
+      self.chosenFolderData(data);
+    });
   };
+
+  // show inbox by default
+  self.goToFolder('Inbox');
 };
 
 ko.applyBindings(new WebmailViewModel());
